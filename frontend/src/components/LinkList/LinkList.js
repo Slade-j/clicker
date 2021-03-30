34b82415f11070f1e66420f04248b660a14ab341
Jason Slade <jason.slade906@gmail.com>
2021-03-30 17:06:54 -0600

    added LinkList dynamic nav componant

---
+++ frontend/src/components/LinkList/LinkList.js
@@ -0,0 +1,21 @@
+// frontend/components/LinkList/LinkList.js
+
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+// dynamic render of links to be re used. navArray must have to:label pares
+function LinkList({ navArray }) {
+  return (
+    <div className="linklist">
+			{navArray.map((item, idx) => {
+				if (idx === 0) {
+					return <NavLink exact to={item}>{navArray[1]}</NavLink>
+				} else if ( 2 % idx === 0) {
+					return <NavLink to={item}>{navArray[idx + 1]}</NavLink>
+				}
+			})}
+    </div>
+  )
+}
+
+export default LinkList;
