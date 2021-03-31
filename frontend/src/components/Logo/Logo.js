b56d087b3b598d49a8ac94351f15615ad3d94eec
Jason Slade <jason.slade906@gmail.com>
2021-03-30 19:21:40 -0600

    added Logo componant

---
+++ frontend/src/components/Logo/Logo.js
@@ -0,0 +1,17 @@
+// frontend/src/components/Logo/Logo.js
+
+import React from 'react';
+
+function Logo() {
+  return (
+    <div className='logo-container'>
+      <div className='circles-container'></div>
+        <div className='blue-circle'></div>
+        <div className='red-circle'></div>
+      <div className='text-container'></div>
+        <h3 className='flicker'>flipper</h3>
+    </div>
+  )
+}
+
+export default Logo;
---
+++ frontend/src/components/TopNav/TopNav.js
@@ -0,0 +1,11 @@
+// frontend/src/components/TopNav/TopNav.js
+
+import React from 'react'
+
+function TopNav() {
+  return (
+    <nav className="topNav">
+
+    </nav>
+  )
+}
