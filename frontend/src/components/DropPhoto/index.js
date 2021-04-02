1ce1f49dc4453e1255436864e4e5e486401d4965
Jason Slade <jason.slade906@gmail.com>
2021-04-01 23:30:28 -0600

    added thumbnail preview functionality to drag and drop with styles

--- frontend/src/components/DropPhoto/index.js
+++ frontend/src/components/DropPhoto/index.js
@@ -1,38 +1,64 @@
 // frontend/src/components/DropPhoto/index.js
-import { useDispatch } from 'react-redux';
-import { addPhotos } from '../../store/photos';
-import './DropPhotos.css';
-
+import { useState } from 'react';
+import {
+  dropZone,
+  or,
+  buttonHolder,
+  labelButton,
+  uploader,
+  thumbNail,
+  thumbContainer,
+  display
+ } from './DropPhoto.module.css'
 
 function DropPhoto() {
-  const dispatch = useDispatch();
+  const [ previews, setPreviews ] = useState({ name: null, url: null });
 
   const handleDrop = (e) => {
     e.preventDefault();
-    e.stopPropigation();
-    const photos = e.dataTransfer.files;
-    console.log(photos);
-    // dispatch(addPhotos(photos));
+    e.stopPropagation();
+    console.log('drop event fired')
+    setPreviews({
+      url: URL.createObjectURL(e.dataTransfer.files[0]),
+      name: e.dataTransfer.files[0].name
+    });
+
+    URL.revokeObjectURL(previews.url);
   }
 
   const handleDragOver = (e) => {
     e.preventDefault();
-    e.stopPropigation();
+    e.stopPropagation();
+    console.log('drag event fired')
     e.dataTransfer.dropEffect = 'copy';
   }
 
+  const handleDragEnter = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    console.log('drag enter event fired');
+  }
+
   return (
-    <div className='dropZone'
+    <div className={previews.url?display:dropZone}
+      onDragEnter={handleDragEnter}
       onDragOver={handleDragOver}
       onDrop = {handleDrop}>
-      <div className='TextHolder'>
-        <h2 className='title'>Drag and drop photos here</h2>
-        <h2 className='or'>or</h2>
-        <div className='button holder'>
-          <button className='uploadButton'>Choose photos to upload</button>
+      {previews.url ?
+        <div className={thumbContainer}>
+          <img src={previews.url} className={thumbNail} />
+          <span>{previews.name}</span>
+          </div> :
+        <div className='TextHolder'>
+          <h2 className='title'>Drag and drop photos here</h2>
+          <h2 className={or}>or</h2>
+          <div className={buttonHolder}>
+            <label htmlFor='file' className={labelButton}>Choose photo from file</label>
+            <input type='file' id='file' className={uploader}></input>
+          </div>
         </div>
+      }
       </div>
-    </div>
   )
 }
 
